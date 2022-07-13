3c1fc63f64fb65b66255d0ff1574c5186b967b3e
Tim Coleman <tjc.coleman@gmail.com>
2022-07-13 11:26:56 -0600

    update item descriptions for correct health

--- lib/factory/item_factory.dart
+++ lib/factory/item_factory.dart
@@ -7,9 +7,9 @@ class ItemFactory {
   static List<Item> create() {
     List<Item> items = [];
     items.add(Potion("Health Potion", "Will heal whoever uses it by 50 health.", 50, 100, 50));
-    items.add(Bow("Bow", "Bow defense increases health by 50.", 25, 75, 20));
-    items.add(Boomerang("Boomerang", "Boomerang defense increases health by 50.", 50, 100, 30));
-    items.add(Wand("Wand", "Wand defense increases health by 50.", 100, 200, 100));
+    items.add(Bow("Bow", "Bow defense increases health by 20.", 25, 75, 20));
+    items.add(Boomerang("Boomerang", "Boomerang defense increases health by 30.", 50, 100, 30));
+    items.add(Wand("Wand", "Wand defense increases health by 100.", 100, 200, 100));
     items.add(Claw("Claw", "Claw defense increases health by 50.", 25, 50, 50));
 
     return items;
